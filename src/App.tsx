a2f22708076ecb0c6ed70a3560944b51f9e8ac3b
Ryan Pals <ryan@hobu.co>
2023-06-08 15:08:09 -0500

    Add click handlers, popups, and legend

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Map } from "./Components/Map";
+import { MapProvider } from "react-map-gl";
 
 import countries from "./countries.json";
 import earthquakes from "./earthquakes.json";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   const queryParams = new URLSearchParams(window.location.search);
   const token = queryParams.get("access_token");
 
   console.log({ earthquakes, countries });
-  if (token) return <Map />;
+  if (token)
+    return (
+      <div className="App">
+        <MapProvider>
+          <Map />
+        </MapProvider>
+      </div>
+    );
   else
     return (
       <div className="App">
--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,6 +1,7 @@
 import { FeatureCollection, GeoJSON } from "geojson";
 import React, { FC, useState, useEffect, useRef } from "react";
-import { Map as MapBoxMap, Source, Layer } from "react-map-gl";
+import { Map as MapBoxMap, Source, Layer, useMap, Popup } from "react-map-gl";
+import mapboxgl from "mapbox-gl";
 
 import countries from "../countries.json";
 import earthquakes from "../earthquakes.json";
@@ -36,9 +37,48 @@ export const Map: FC = () => {
     },
   } as const;
 
+  const [description, setDescription] = useState<string | null>(null);
+  const [coordinates, setCoordinates] = useState<number[] | null>(null);
+  const [popup, setPopup] = useState<boolean>(false);
+
+  const { map } = useMap();
+  useEffect(() => {
+    if (map) {
+      map.on("mouseenter", "earthquakes-layer", () => {
+        map.getCanvas().style.cursor = "pointer";
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on("mouseleave", "earthquakes-layer", () => {
+        map.getCanvas().style.cursor = "";
+      });
+      map.on("click", "earthquakes-layer", (e) => {
+        console.log(e);
+        // Copy coordinates array.
+        //@ts-ignore
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        //@ts-ignore
+        const description = e.features[0].properties.description;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        setCoordinates(coordinates);
+        setDescription(description);
+        setPopup(true);
+      });
+    }
+  }, [map]);
+
+  console.log(coordinates, description, popup);
   return (
     <MapBoxMap
       mapLib={import("mapbox-gl")}
+      id="map"
       initialViewState={{
         longitude: -100,
         latitude: 40,
@@ -47,6 +87,22 @@ export const Map: FC = () => {
       style={{ width: 900, height: 600 }}
       //mapStyle="mapbox://styles/mapbox/streets-v9" // looks better without the base layer IMO
     >
+      {popup && coordinates && (
+        <Popup
+          longitude={coordinates[0]}
+          latitude={coordinates[1]}
+          closeButton={true}
+          closeOnClick={false}
+          onClose={() => {
+            setPopup(false);
+            setCoordinates(null);
+            setDescription(null);
+          }}
+          anchor="top"
+        >
+          Testing
+        </Popup>
+      )}
       <Source id="countries-source" type="geojson" data={countries as GeoJSON}>
         <Layer {...fillStyle} />
         <Layer {...outlineStyle} />
@@ -54,6 +110,7 @@ export const Map: FC = () => {
       <Source id="earthquake-source" type="geojson" data={earthquakes as any}>
         <Layer {...earthquakeLayerStyle} />
       </Source>
+      <div className="Legend">hello world</div>
     </MapBoxMap>
   );
 };
